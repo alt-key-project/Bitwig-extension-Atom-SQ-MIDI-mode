f7d191e8940022b7523553c8f0b8f3b96acc6286
Morgan Johansson <morgan.johansson@gmail.com>
2022-08-30 19:36:58 +0200

    Project generated by BitWig

---
+++ src/main/java/com/altkeyproject/AtomSQExtension.java
@@ -0,0 +1,67 @@
+package com.altkeyproject;
+
+import com.bitwig.extension.api.util.midi.ShortMidiMessage;
+import com.bitwig.extension.callback.ShortMidiMessageReceivedCallback;
+import com.bitwig.extension.controller.api.ControllerHost;
+import com.bitwig.extension.controller.api.Transport;
+import com.bitwig.extension.controller.ControllerExtension;
+
+public class AtomSQExtension extends ControllerExtension
+{
+   protected AtomSQExtension(final AtomSQExtensionDefinition definition, final ControllerHost host)
+   {
+      super(definition, host);
+   }
+
+   @Override
+   public void init()
+   {
+      final ControllerHost host = getHost();      
+
+      mTransport = host.createTransport();
+      host.getMidiInPort(0).setMidiCallback((ShortMidiMessageReceivedCallback)msg -> onMidi0(msg));
+      host.getMidiInPort(0).setSysexCallback((String data) -> onSysex0(data));
+
+      // TODO: Perform your driver initialization here.
+      // For now just show a popup notification for verification that it is running.
+      host.showPopupNotification("Atom SQ Initialized");
+   }
+
+   @Override
+   public void exit()
+   {
+      // TODO: Perform any cleanup once the driver exits
+      // For now just show a popup notification for verification that it is no longer running.
+      getHost().showPopupNotification("Atom SQ Exited");
+   }
+
+   @Override
+   public void flush()
+   {
+      // TODO Send any updates you need here.
+   }
+
+   /** Called when we receive short MIDI message on port 0. */
+   private void onMidi0(ShortMidiMessage msg) 
+   {
+      // TODO: Implement your MIDI input handling code here.
+   }
+
+   /** Called when we receive sysex MIDI message on port 0. */
+   private void onSysex0(final String data) 
+   {
+      // MMC Transport Controls:
+      if (data.equals("f07f7f0605f7"))
+            mTransport.rewind();
+      else if (data.equals("f07f7f0604f7"))
+            mTransport.fastForward();
+      else if (data.equals("f07f7f0601f7"))
+            mTransport.stop();
+      else if (data.equals("f07f7f0602f7"))
+            mTransport.play();
+      else if (data.equals("f07f7f0606f7"))
+            mTransport.record();
+   }
+
+   private Transport mTransport;
+}
---
+++ src/main/java/com/altkeyproject/AtomSQExtensionDefinition.java
@@ -0,0 +1,99 @@
+package com.altkeyproject;
+import java.util.UUID;
+
+import com.bitwig.extension.api.PlatformType;
+import com.bitwig.extension.controller.AutoDetectionMidiPortNamesList;
+import com.bitwig.extension.controller.ControllerExtensionDefinition;
+import com.bitwig.extension.controller.api.ControllerHost;
+
+public class AtomSQExtensionDefinition extends ControllerExtensionDefinition
+{
+   private static final UUID DRIVER_ID = UUID.fromString("064b2476-088d-4fa0-a4c2-ba1092d869cd");
+   
+   public AtomSQExtensionDefinition()
+   {
+   }
+
+   @Override
+   public String getName()
+   {
+      return "Atom SQ";
+   }
+   
+   @Override
+   public String getAuthor()
+   {
+      return "Alt Key Project";
+   }
+
+   @Override
+   public String getVersion()
+   {
+      return "0.1";
+   }
+
+   @Override
+   public UUID getId()
+   {
+      return DRIVER_ID;
+   }
+   
+   @Override
+   public String getHardwareVendor()
+   {
+      return "Presonus";
+   }
+   
+   @Override
+   public String getHardwareModel()
+   {
+      return "Atom SQ";
+   }
+
+   @Override
+   public int getRequiredAPIVersion()
+   {
+      return 17;
+   }
+
+   @Override
+   public int getNumMidiInPorts()
+   {
+      return 1;
+   }
+
+   @Override
+   public int getNumMidiOutPorts()
+   {
+      return 1;
+   }
+
+   @Override
+   public void listAutoDetectionMidiPortNames(final AutoDetectionMidiPortNamesList list, final PlatformType platformType)
+   {
+      if (platformType == PlatformType.WINDOWS)
+      {
+         // TODO: Set the correct names of the ports for auto detection on Windows platform here
+         // and uncomment this when port names are correct.
+         // list.add(new String[]{"Input Port 0"}, new String[]{"Output Port 0"});
+      }
+      else if (platformType == PlatformType.MAC)
+      {
+         // TODO: Set the correct names of the ports for auto detection on Windows platform here
+         // and uncomment this when port names are correct.
+         // list.add(new String[]{"Input Port 0"}, new String[]{"Output Port 0"});
+      }
+      else if (platformType == PlatformType.LINUX)
+      {
+         // TODO: Set the correct names of the ports for auto detection on Windows platform here
+         // and uncomment this when port names are correct.
+         // list.add(new String[]{"Input Port 0"}, new String[]{"Output Port 0"});
+      }
+   }
+
+   @Override
+   public AtomSQExtension createInstance(final ControllerHost host)
+   {
+      return new AtomSQExtension(this, host);
+   }
+}
